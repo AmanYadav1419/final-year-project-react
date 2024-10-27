c3a0be22ed71ed348e6e67ba6720a23bb1449629
unknown <amanyadav16544@gmail.com>
2024-10-27 18:15:24 +0530

    created & imported user Routes

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,12 @@ const app = express();
 // created the server port , imported from .env file
 const port = process.env.PORT;
 
+// importing routes
+import userRoutes from './routes/userRoutes.js'
+
+// using routes
+app.use("/api/user", userRoutes)
+
 // starting or checking running correctly or not
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+// all user routes are written in here
+
+import express from "express";
+
+// create a router
+const router = express.Router();
+
+// export the router
+export default router;
