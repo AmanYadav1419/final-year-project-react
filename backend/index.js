a1ce3cdcc49fae2c2e5c55f0bcdc8e9e8dc223da
unknown <amanyadav16544@gmail.com>
2024-10-27 18:43:18 +0530

    created & connected mongodb database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDb from "./database/db.js";
 
 dotenv.config();
 
@@ -10,12 +11,14 @@ const app = express();
 const port = process.env.PORT;
 
 // importing routes
-import userRoutes from './routes/userRoutes.js'
+import userRoutes from "./routes/userRoutes.js";
 
 // using routes
-app.use("/api/user", userRoutes)
+app.use("/api/user", userRoutes);
 
 // starting or checking running correctly or not
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
+  // function of connection of database
+  connectDb();
 });
