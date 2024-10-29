44b2029f84f931847059167e75c8716cdbef3764
unknown <amanyadav16544@gmail.com>
2024-10-29 10:51:46 +0530

    ui part of login page is done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+const headingText = "Login to RhythmNet";
 
 const Login = () => {
   return (
-    <div>
-        Login
-    </div>
-  )
-}
+    <div className="flex items-center justify-center h-screen max-h-screen">
+      <div className="bg-black text-white p-8 rounded-lg shadow-lg max-w-md w-full">
+        {/* heading text */}
+        <h2 className="text-3xl font-semibold text-center mb-8">
+          {headingText}
+        </h2>
 
-export default Login
+        {/* form section */}
+        <form className="mt-8">
+
+          {/* email label and input */}
+          <div className="mb-4">
+            <label className="block text-sm font-medium mb-1">
+              Email or Username
+            </label>
+            <input
+              type="email"
+              placeholder="Email or Username"
+              className="auth-input"
+              required
+            />
+          </div>
+
+          {/* password label and input */}
+          <div className="mb-4">
+            <label className="block text-sm font-medium mb-1">Password</label>
+            <input
+              type="password"
+              placeholder="Password"
+              className="auth-input"
+              required
+            />
+          </div>
+
+          {/* btn for submit or Login */}
+          <button className="auth-btn">Login</button>
+        </form>
+
+        {/* this div for redirect to register page if user don't have account*/}
+        <div className="text-center mt-6">
+          <Link
+            to="/register"
+            className="text-sm text-gray-400 hover:text-gray-300"
+          >
+            Don't have account? Register here 
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
