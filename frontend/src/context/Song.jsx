88bc80c211b8fd386692489ad93fb93cb867a5a3
unknown <amanyadav16544@gmail.com>
2024-11-08 20:56:35 +0530

    added fetchSongs fn and added states

--- frontend/src/context/Song.jsx
+++ frontend/src/context/Song.jsx
@@ -1,16 +1,43 @@
-import { createContext, useContext } from "react";
+import axios from "axios";
+import { createContext, useContext, useEffect, useState } from "react";
 
-// created a context 
+// created a context
 const SongContext = createContext();
 
-// created a song provider 
-export const SongProvider = ({children}) => {
-    return <SongContext.Provider value={}>
-        {children}
-    </SongContext.Provider>
-}
+// created a song provider
+export const SongProvider = ({ children }) => {
+  // after fetching all store it in
+  const [songs, setSongs] = useState([]);
+  // when fetched the songs the loading bar we can sho for that this is state
+  const [songloading, setSongLoading] = useState(true);
+  // for button loading
+  const [loading, setLoading] = useState(false);
 
-// export songData to use further in different files 
-export const SongData = () =>{
-    useContext(SongContext)
-}
+  // to fetch all the songs
+  async function fetchSongs() {
+    try {
+      // get all the songs
+      const { data } = await axios.get("/api/song/all");
+
+      // send the songs data value to the song state
+      setSongs(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  //  useeffect to make the fetch function call and for rendering of songs
+  useEffect(() => {
+    // call the fetch function, so when page load/reload the fetch function calls
+    fetchSongs();
+  }, []);
+
+  return (
+    <SongContext.Provider value={{ songs }}>{children}</SongContext.Provider>
+  );
+};
+
+// export songData to use further in different files
+export const SongData = () => {
+  useContext(SongContext);
+};
