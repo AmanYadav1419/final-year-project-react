0e5f0f5b4fb9e771a66a5e9fef54443bff755b3f
unknown <amanyadav16544@gmail.com>
2024-11-20 18:53:15 +0530

    showing songs to ui with two btn's on hover

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "../components/Layout";
 import { SongData } from "../context/Song";
 import AlbumItem from "../components/AlbumItem";
+import SongItem from "../components/SongItem";
 
 const Home = () => {
   // import necesary data from Song Context
@@ -14,14 +15,38 @@ const Home = () => {
     <Layout>
       <div className="mb-4">
         <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
-        {/* this div to map all the songs and data  */}
-        {/* then show the data or songs or albums */}
+        {/* this div to map all the albums and data  */}
+        {/* then show the data or albums */}
         {/* overflow auto means only scrollable to x-direction */}
         <div className="flex overflow-auto">
           {albums &&
             albums.map((e, i) => (
               // render or display the albumItem for each album
               <AlbumItem
+                key={i}
+                // send all respective the data to the AlbumItem component
+                image={e.thumbnail.url}
+                name={e.title}
+                desc={e.description}
+                id={e._id}
+              />
+            ))}
+        </div>
+      </div>
+
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">
+          Today's Biggest Hits.!
+        </h1>
+        
+        {/* this div to map all the songs  */}
+        {/* then show the data or songs */}
+        {/* overflow auto means only scrollable to x-direction */}
+        <div className="flex overflow-auto">
+          {albums &&
+            albums.map((e, i) => (
+              // render or display the albumItem for each album
+              <SongItem
                 key={i}
                 // send all respective the data to the AlbumItem component
                 image={e.thumbnail.url}
