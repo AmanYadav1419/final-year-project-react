136a51b194aa7bb6021ed27ca776667c076ea50a
unknown <amanyadav16544@gmail.com>
2024-11-20 18:06:03 +0530

    created all, music, podcasts, playlist section to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,54 +10,76 @@ const Navbar = () => {
   const { logoutUser } = UserData();
 
   return (
-    <div className="w-full flex justify-between items-center font-semibold">
-      {/* in this div arrow images of navbar */}
-      <div className="flex items-center">
-        <img
-          src={assets.arrow_left}
-          className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
-          alt=""
-          // on click navigate to back
-          onClick={() => navigate(-1)}
-        />
+    <>
+      <div className="w-full flex justify-between items-center font-semibold">
+        {/* in this div arrow images of navbar */}
+        <div className="flex items-center">
+          <img
+            src={assets.arrow_left}
+            className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
+            alt=""
+            // on click navigate to back
+            onClick={() => navigate(-1)}
+          />
 
-        <img
-          src={assets.arrow_right}
-          className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
-          alt=""
-          // on click navigate to next
-          onClick={() => navigate(+1)}
-        />
-      </div>
+          <img
+            src={assets.arrow_right}
+            className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
+            alt=""
+            // on click navigate to next
+            onClick={() => navigate(+1)}
+          />
+        </div>
 
-      <div className="flex items-center gap-4">
-        {/* Explore premium text */}
-        <p
-          className="bg-white text-black text-[15px] px-4 py-1 
+        <div className="flex items-center gap-4">
+          {/* Explore premium text */}
+          <p
+            className="bg-white text-black text-[15px] px-4 py-1 
         rounded-2xl hidden md:block cursor-pointer"
-        >
-          Explore Premium
-        </p>
+          >
+            Explore Premium
+          </p>
 
-        {/* Install app text */}
-        <p
-          className="bg-white text-black text-[15px] px-4 py-1 
+          {/* Install app text */}
+          <p
+            className="bg-white text-black text-[15px] px-4 py-1 
         rounded-2xl hidden md:block cursor-pointer"
-        >
-          Install App
-        </p>
+          >
+            Install App
+          </p>
 
-        {/* Logout text and onclick handler */}
-        <p
-          className="bg-white text-black text-[15px] px-4 py-1 
+          {/* Logout text and onclick handler */}
+          <p
+            className="bg-white text-black text-[15px] px-4 py-1 
         rounded-2xl cursor-pointer"
-          // onclick of the text logout handler function will work
-          onClick={logoutUser}
+            // onclick of the text logout handler function will work
+            onClick={logoutUser}
+          >
+            Logout
+          </p>
+        </div>
+      </div>
+
+      {/* this div for showing more buttons */}
+      <div className="flex items-center gap-2">
+        <p className="bg-white text-black px-4 py-1 rounded-2xl cursor-pointer">
+          All
+        </p>
+        <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer hidden md:block">
+          Music
+        </p>
+        <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer hidden md:block">
+          Podcasts
+        </p>
+        <p
+          // onclick handler to navigate to respective route
+          onClick={() => navigate("/playlist")}
+          className="bg-black px-4 py-1 rounded-2xl cursor-pointer md:hidden"
         >
-          Logout
+          Playlist
         </p>
       </div>
-    </div>
+    </>
   );
 };
 
