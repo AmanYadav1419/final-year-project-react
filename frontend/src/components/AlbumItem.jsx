d0e2a0f2ca7dac0336176c9679a70af5b24dccf3
unknown <amanyadav16544@gmail.com>
2024-11-20 18:15:06 +0530

    done the basic rendering of album in home page

---
+++ frontend/src/components/AlbumItem.jsx
@@ -0,0 +1,11 @@
+// this file for rendering each album present in the data or in user data
+
+import React from 'react'
+
+const AlbumItem = () => {
+  return (
+    <div>AlbumItem</div>
+  )
+}
+
+export default AlbumItem
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { SongData } from "../context/Song";
+import AlbumItem from "../components/AlbumItem";
 
 const Home = () => {
+  // import necesary data from Song Context
+  const { songs, albums } = SongData();
+
+  // const navigate =
+
   return (
     // wrap home page with layout
-    <Layout>HomePage</Layout>
+    <Layout>
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        {/* this div to map all the songs and data  */}
+        {/* then show the data or songs or albums */}
+        {/* overflow auto means only scrollable to x-direction */}
+        <div className="flex overflow-auto">
+          {albums &&
+            albums.map((e, i) => (
+              // render or display the albumItem for each album
+              <AlbumItem key={i} />
+            ))}
+        </div>
+      </div>
+    </Layout>
   );
 };
 
