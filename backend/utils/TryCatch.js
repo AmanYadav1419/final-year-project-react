45fde06a9ef6d313e9fe936be2cd01f80631ab00
AmanYadav1419 <amanyadav16544@gmail.com>
2024-12-05 14:35:21 +0530

    removed comments from utils folder, rewrite code , fixed bug(10)

--- backend/utils/TryCatch.js
+++ backend/utils/TryCatch.js
@@ -1,15 +1,8 @@
-// for writting the try catch block again and again
-// we can create a seperate file from that we can write try catch block
-// so that it removes the writting the same code again and again, & Save time.
-
-// function for try catch block
 const TryCatch = (handler) => {
   return async (req, res, next) => {
     try {
-      // we send the handler with that req,res,next
       await handler(req, res, next);
     } catch (error) {
-      // if error return status 500 with that error message
       res.status(500).json({
         message: error.message,
       });
