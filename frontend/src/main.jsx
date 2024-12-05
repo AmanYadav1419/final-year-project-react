efae6cd46ae15886a4401df6245e46bb4ce439ad
AmanYadav1419 <amanyadav16544@gmail.com>
2024-12-05 14:08:40 +0530

    removed comments from index.css & main.jsx, rewrite code,fixed bug(7)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
+import "./index.css";
 import { UserProvider } from "./context/User.jsx";
 import { SongProvider } from "./context/Song.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* wrap the app file with the user context to acess data globally */}
     <UserProvider>
-      {/* wrap the app file with song context to acess data globally */}
       <SongProvider>
         <App />
       </SongProvider>
