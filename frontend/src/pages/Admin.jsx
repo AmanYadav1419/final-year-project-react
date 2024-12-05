d7f9079c8424fa7bc5fddf3028488dfe7940e814
AmanYadav1419 <amanyadav16544@gmail.com>
2024-12-05 14:06:12 +0530

    removed comments from pages files, rewrite code , fixed bug(6)

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -5,10 +5,7 @@ import { SongData } from "../context/Song";
 import { MdDelete } from "react-icons/md";
 
 const Admin = () => {
-  // add a condition only who had admin acess can acess this admin page
   const { user } = UserData();
-
-  // to get the respected data from Song.jsx
   const {
     albums,
     songs,
@@ -18,224 +15,159 @@ const Admin = () => {
     addThumbnail,
     deleteSong,
   } = SongData();
-
-  // created a navigate function to navigate from one route to another route
   const navigate = useNavigate();
 
-  // if user is not admin then return to home page
   if (user && user.role !== "admin") return navigate("/");
 
-  // for title
   const [title, setTitle] = useState("");
-  // for description
   const [description, setDescription] = useState("");
-  // for singer
-  const [singer, setSinger] = useState("");
-  // for album
-  const [album, setAlbum] = useState("");
-  // for file
   const [file, setFile] = useState(null);
+  const [singer, setSinger] = useState("");
+  const [album, setAlbum] = useState("");
 
-  // on change handler for file
   const fileChangeHandler = (e) => {
-    // get the very first file
     const file = e.target.files[0];
-    // send or setted the file to the state
     setFile(file);
   };
 
-  // on submit of form handler
   const addAlbumHandler = (e) => {
-    // firstly prevent the default behavoiur of form i.e page reload
     e.preventDefault();
 
-    // get the form data
     const formData = new FormData();
 
-    // append all data
     formData.append("title", title);
     formData.append("description", description);
     formData.append("file", file);
-
-    // call the add album function, and send formData to function with that all state functions
     addAlbum(formData, setTitle, setDescription, setFile);
   };
 
-  // on submit of form handler for song
   const addSongHandler = (e) => {
-    // firstly prevent the default behavoiur of form i.e page reload
     e.preventDefault();
 
-    // get the form data
     const formData = new FormData();
 
-    // append all data
     formData.append("title", title);
     formData.append("description", description);
     formData.append("singer", singer);
     formData.append("album", album);
     formData.append("file", file);
-
-    // call the add Song function, and send formData to function with that all state functions
     addSong(formData, setTitle, setDescription, setFile, setSinger, setAlbum);
   };
 
-  // on submit of form handler for song thumbnail
   const addThumbnailHandler = (id) => {
-    // get the form data
     const formData = new FormData();
-
-    // append all the data
     formData.append("file", file);
 
-    // call the add Song thumbnail function, and send formData to function with that all state functions
     addThumbnail(id, formData, setFile);
   };
 
-  // on submit of form handler delete handler work
   const deleteHandler = (id) => {
-    // first give the confirm message modal popup
-    // if it is true then only deletes the song by id
     if (confirm("are you sure you want to delete this song")) {
       deleteSong(id);
     }
   };
-
   return (
     <div className="min-h-screen bg-[#212121] text-white p-8">
-      {/* redirect or send to home page */}
       <Link
         to="/"
         className="bg-green-500 text-white font-bold py-2 px-4 rounded-full"
       >
-        Go to Home Page
+        Go to home page
       </Link>
-
       <h2 className="text-2xl font-bold mb-6 mt-6">Add Album</h2>
 
       <form
-        // on submit of form handler
         onSubmit={addAlbumHandler}
         className="bg-[#181818] p-6 rounded-lg shadow-lg"
       >
-        {/* for title */}
         <div className="mb-4">
           <label className="block text-sm font-medium mb-1">Title</label>
           <input
             type="text"
             placeholder="Title"
             className="auth-input"
-            required
-            // value as title
             value={title}
-            // on typing the title value should be print or written
             onChange={(e) => setTitle(e.target.value)}
+            required
           />
         </div>
-
-        {/* for description */}
         <div className="mb-4">
-          <label className="block text-sm font-medium mb-1">Description</label>
+          <label className="block text-sm font-medium mb-1">Descripiton</label>
           <input
             type="text"
-            placeholder="Description"
+            placeholder="Descripiton"
             className="auth-input"
-            required
-            // value as description
             value={description}
-            // on typing the description value should be print or written
             onChange={(e) => setDescription(e.target.value)}
+            required
           />
         </div>
 
-        {/* for File */}
         <div className="mb-4">
           <label className="block text-sm font-medium mb-1">Thumbnail</label>
           <input
             type="file"
-            // it should only accept image
-            accept="image/*"
             className="auth-input"
-            // added onchnage event file handler
+            accept="image/*"
             onChange={fileChangeHandler}
             required
           />
         </div>
 
-        {/* button to add album */}
         <button
-          // until loading is there for adding album disabled the button
           disabled={loading}
           className="auth-btn"
           style={{ width: "100px" }}
         >
-          {/* based on condition change the button text */}
-          {loading ? "Please Wait!" : "Add"}
+          {loading ? "Please Wait..." : "Add"}
         </button>
       </form>
 
-      {/* to add song  */}
-      <h2 className="text-2xl font-bold mb-6 mt-6">Add Song</h2>
+      <h2 className="text-2xl font-bold mb-6 mt-6">Add Songs</h2>
 
       <form
-        // added on submit handler
         onSubmit={addSongHandler}
         className="bg-[#181818] p-6 rounded-lg shadow-lg"
       >
-        {/* for title */}
         <div className="mb-4">
           <label className="block text-sm font-medium mb-1">Title</label>
           <input
             type="text"
             placeholder="Title"
             className="auth-input"
-            required
-            // value as title
             value={title}
-            // on typing the title value should be print or written
             onChange={(e) => setTitle(e.target.value)}
+            required
           />
         </div>
-
-        {/* for description */}
         <div className="mb-4">
-          <label className="block text-sm font-medium mb-1">Description</label>
+          <label className="block text-sm font-medium mb-1">Descripiton</label>
           <input
             type="text"
-            placeholder="Description"
+            placeholder="Descripiton"
             className="auth-input"
-            required
-            // value as description
             value={description}
-            // on typing the description value should be print or written
             onChange={(e) => setDescription(e.target.value)}
+            required
           />
         </div>
-
-        {/* for Singer */}
         <div className="mb-4">
           <label className="block text-sm font-medium mb-1">Singer</label>
           <input
             type="text"
             placeholder="Singer"
             className="auth-input"
-            required
-            // value as singer
             value={singer}
-            // on typing the singer value should be print or written
             onChange={(e) => setSinger(e.target.value)}
+            required
           />
         </div>
 
-        {/* map all the albums in this */}
         <select
           className="auth-input"
-          // value as album
           value={album}
-          // onchange event handler
           onChange={(e) => setAlbum(e.target.value)}
         >
-          {/* initially show the dummy option */}
           <option value="">Choose Album</option>
           {albums &&
             albums.map((e, i) => (
@@ -245,46 +177,32 @@ const Admin = () => {
             ))}
         </select>
 
-        {/* for File i.e audio*/}
         <div className="mb-4">
-          <label className="block text-sm font-medium mb-1">Audio</label>
+          <label className="block text-sm font-medium mb-1">audio</label>
           <input
             type="file"
-            // it should only accept audio
-            accept="audio/*"
             className="auth-input"
-            // added onchnage event file handler
+            accept="audio/*"
             onChange={fileChangeHandler}
             required
           />
         </div>
 
-        {/* button to add album */}
         <button
-          // until loading is there for adding album disabled the button
           disabled={loading}
           className="auth-btn"
           style={{ width: "100px" }}
         >
-          {/* based on condition change the button text */}
-          {loading ? "Please Wait!" : "Add"}
+          {loading ? "Please Wait..." : "Add"}
         </button>
       </form>
 
-      {/* to show all the added songs  */}
       <div className="mt-8">
         <h3 className="text-xl font-semibold mb-4">Added Songs</h3>
-        <div
-          className="flex justify-center md:justify-start gap-2 
-          items-center flex-wrap"
-        >
-          {/* in that maps all the songs */}
+        <div className="flex justify-center md:justify-start gap-2 items-center flex-wrap">
           {songs &&
             songs.map((e, i) => (
-              // render a div for each song
               <div key={i} className="bg-[#181818] p-4 rounded-lg shadow-md">
-                {/* for showing thumbnail showing the condition, */}
-                {/* if thumbnail then only show the image, if not show a div */}
                 {e.thumbnail ? (
                   <img
                     src={e.thumbnail.url}
@@ -292,17 +210,10 @@ const Admin = () => {
                     className="mr-1 w-52 h-52"
                   />
                 ) : (
-                  // if thumbnail is not present then show this div
-                  <div className="flex flex-col items-center justify-center gap-2">
-                    <input
-                      type="file"
-                      // added onchnage event file handler
-                      onChange={fileChangeHandler}
-                    />
+                  <div className="flex flex-col justify-center items-center gap-2">
+                    <input type="file" onChange={fileChangeHandler} />
                     <button
-                      // add on click submit handler for adding thumbnail
-                      // _id is the song id
-                      onClick={() => addThumbnail(e._id)}
+                      onClick={() => addThumbnailHandler(e._id)}
                       className="bg-green-500 text-white px-2 py-1 rounded"
                     >
                       Add Thumbnail
@@ -310,19 +221,11 @@ const Admin = () => {
                   </div>
                 )}
 
-                {/* song title */}
                 <h4 className="text-lg font-bold">{e.title}</h4>
-
-                {/* song singer */}
                 <h4 className="text-sm text-gray-500">{e.singer}</h4>
-
-                {/* song description */}
                 <h4 className="text-sm text-gray-500">{e.description}</h4>
 
-                {/* btn for delete */}
                 <button
-                  // delete song handler
-                  // _id is the song id
                   onClick={() => deleteHandler(e._id)}
                   className="px-3 py-1 bg-red-500 text-white rounded"
                 >
--- frontend/src/pages/PlayList.jsx
+++ frontend/src/pages/PlayList.jsx
@@ -1,84 +1,58 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../components/Layout";
 import { SongData } from "../context/Song";
+import { assets } from "../assets/assets";
 import { FaBookmark, FaPlay } from "react-icons/fa";
 import { UserData } from "../context/User";
 
 const PlayList = ({ user }) => {
-  // import all the neccessary data from Song context
   const { songs, setSelectedSong, setIsPlaying } = SongData();
 
-  // import all the neccessarry data from User context
-  const { addToPlaylist } = UserData();
-
-  // state for myplaylist initilise with empty array
   const [myPlaylist, setMyPlaylist] = useState([]);
 
-  // use effect
   useEffect(() => {
-    // first check the condition
     if (songs && user && Array.isArray(user.playlist)) {
-      // then filter the songs based on id
-      // id convert it into string
-      const filterSongs = songs.filter((e) =>
+      const filteredSongs = songs.filter((e) =>
         user.playlist.includes(e._id.toString())
       );
-      setMyPlaylist(filterSongs);
+      setMyPlaylist(filteredSongs);
     }
-    // when change in songs or in user then re excutes
   }, [songs, user]);
 
-  // on click handler for selected song and set playing,
-  // to play/pause the song
-  const onClickHandler = (id) => {
-    // set song via its id to the selected song
+  const onclickHander = (id) => {
     setSelectedSong(id);
-
-    // and make the set is playing to start the song playing
     setIsPlaying(true);
   };
 
-  // on click handler for saving/remoing the song to playlist
+  const { addToPlaylist } = UserData();
+
   const savePlayListHandler = (id) => {
-    // send the id value to the context function
     addToPlaylist(id);
   };
 
   return (
     <Layout>
       <div className="mt-10 flex gap-8 flex-col md:flex-row md:items-center">
-        {/* map the playlist  */}
-        {
-          // firstly show the thumbnail of the first song of playlist
-          // first check had myPlaylist or not
-          // if had then the first song of the playlist
-          myPlaylist && myPlaylist[0] ? (
-            // then show the first song thumbnail of playlist
-            <img
-              src={myPlaylist[0].thumbnail.url}
-              className="w-48 rounded" 
-              alt=""
-            />
-          ) : (
-            // if we dont have any playlist or song and thumbnail
-            // then show the dummy image
-            <img
-              src="https://via.placeholder.com/250"
-              className="w-48 rounded"
-              alt=""
-            />
-          )
-        }
+        {myPlaylist && myPlaylist[0] ? (
+          <img
+            src={myPlaylist[0].thumbnail.url}
+            className="w-48 rounded"
+            alt=""
+          />
+        ) : (
+          <img
+            src="https://via.placeholder.com/250"
+            className="w-48 rounded"
+            alt=""
+          />
+        )}
 
-        {/* to show the playlist name with user name */}
         <div className="flex flex-col">
           <p>Playlist</p>
           <h2 className="text-3xl font-bold mb-4 md:text-5xl">
-            {user.name} Playlist
+            {user.name} PlayList
           </h2>
-
-          {/* to show fav song with spotify logo img */}
-          <h4>Your Favourate Songs</h4>
+          <h4>Your Favourate songs</h4>
           <p className="mt-1">
             <img
               src={assets.spotify_logo}
@@ -88,71 +62,41 @@ const PlayList = ({ user }) => {
           </p>
         </div>
       </div>
-
-      {/* to show the playlist in table format */}
-      <div
-        className="grid grid-cols-3 sm:grid-cols-4 
-      mt-10 mb-4 pl-2 text-[#a7a7a7]"
-      >
+      <div className="grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7]">
         <p>
           <b className="mr-4">#</b>
         </p>
-        {/* to show the artist name */}
         <p>Artist</p>
-        {/* to show the descrption  */}
         <p className="hidden sm:block">Description</p>
         <p className="text-center">Actions</p>
       </div>
-
-      {/* horizontal line */}
       <hr />
-
-      {/* if we had myplaylist then map the myplaylist */}
       {myPlaylist &&
         myPlaylist.map((e, i) => (
           <div
-            className="grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 
-            pl-2 text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer"
-            // pass key as i i.e index
+            className="grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer"
             key={i}
           >
             <p className="text-white">
-              <b className="mr-4 text-[#a7a7a7]">
-                {/* i + 1 i.e means the first song */}
-                {i + 1}
-              </b>
-              {/* also add img as the thumbnail to show the song */}
+              <b className="mr-4 text-[#a7a7a7]">{i + 1}</b>
               <img src={e.thumbnail.url} className="inline w-10 mr-5" alt="" />
-              // to show the title of the song
               {e.title}
             </p>
-
-            {/* to show the singer name */}
             <p className="text-[15px]">{e.singer}</p>
-            {/* to show the song description and only show the first 20 words */}
             <p className="text-[15px] hidden sm:block">
               {e.description.slice(0, 20)}...
             </p>
-            {/* to show the bookmark and play icons/buttons */}
             <p className="flex justify-center items-center gap-5">
               <p
                 className="text-[15px] text-center"
-                // on click handler for save song to playlist
-                // it will send the element id to the handler
-                // to set and save the corrected song
                 onClick={() => savePlayListHandler(e._id)}
               >
-                {/* show the bookmark icon */}
                 <FaBookmark />
               </p>
-
               <p
                 className="text-[15px] text-center"
-                // onclick handler and it will send the element id to the handler
-                // to set and played the corrected song
-                onClick={() => onClickHandler(e._id)}
+                onClick={() => onclickHander(e._id)}
               >
-                {/* show the play icon */}
                 <FaPlay />
               </p>
             </p>
